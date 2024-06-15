01ba7542227bb2ef4be650d887181c66f791f9a9
Ericksen77 <c14210151@john.petra.ac.id>
2024-06-15 15:05:39 +0700

    getCountBook

--- booking.py
+++ booking.py
@@ -14,36 +14,52 @@ class BookingService:
         return bookings
     
     @rpc
-    def add_booking_hotel(self,  user_id, type, total_price, asuransi_id, hotel_name,
+    def add_booking_hotel(self,  user_id, type, total_price, asuransi_id, provider_name,
                     room_type, check_in_date, check_out_date,number_of_rooms):
         response = self.database.add_booking_hotel(
-        user_id=user_id, type=type, total_price=total_price, asuransi_id=asuransi_id, hotel_name=hotel_name,
+        user_id=user_id, type=type, total_price=total_price, asuransi_id=asuransi_id, provider_name=provider_name,
         room_type=room_type, check_in_date=check_in_date, check_out_date=check_out_date,number_of_rooms=number_of_rooms
         )
         return response
     @rpc
-    def add_booking_airline(self,  user_id, type, total_price, asuransi_id, flight_id):
+    def add_booking_airline(self,  user_id, type, total_price, asuransi_id, flight_id, provider_name):
         response = self.database.add_booking_airline(
-        user_id=user_id, type=type, total_price=total_price, asuransi_id=asuransi_id, flight_id=flight_id
+        user_id=user_id, type=type, total_price=total_price, asuransi_id=asuransi_id, flight_id=flight_id, provider_name=provider_name
         )
         return response
     @rpc
-    def add_booking_rental(self,  user_id, type, total_price, asuransi_id, rental_provider_name,car_id,pick_up_date,return_date,pick_up_location,return_location,is_with_driver):
+    def add_booking_rental(self,  user_id, type, total_price, asuransi_id, provider_name,car_id,pick_up_date,return_date,pick_up_location,return_location,is_with_driver):
         response = self.database.add_booking_rental(
-        user_id=user_id, type=type, total_price=total_price, asuransi_id=asuransi_id, rental_provider_name=rental_provider_name,
+        user_id=user_id, type=type, total_price=total_price, asuransi_id=asuransi_id, provider_name=provider_name,
                     car_id=car_id, pick_up_date=pick_up_date, return_date=return_date,pick_up_location=pick_up_location,return_location=return_location,
                     is_with_driver=is_with_driver)
         return response
     @rpc
-    def add_booking_attraction(self,  user_id, type, total_price, asuransi_id, attraction_provider_name,
+    def add_booking_attraction(self,  user_id, type, total_price, asuransi_id, provider_name,
                     paket_attraction_id, visit_date, number_of_tickets):
         response = self.database.add_booking_attraction(
-        user_id=user_id, type=type, total_price=total_price, asuransi_id=asuransi_id, attraction_provider_name=attraction_provider_name,
+        user_id=user_id, type=type, total_price=total_price, asuransi_id=asuransi_id, provider_name=provider_name,
         paket_attraction_id=paket_attraction_id, visit_date=visit_date, number_of_tickets=number_of_tickets)
         return response
     @rpc
     def edit_booking(self,status,booking_id):
         response = self.database.edit_booking(status = status, booking_id = booking_id)
+        return response
+    @rpc
+    def getCountBookHotel(self):
+        response = self.database.getCountBookHotel()
+        return response
+    @rpc
+    def getCountBookAirline(self):
+        response = self.database.getCountBookAirline()
+        return response
+    @rpc
+    def getCountBookRental(self):
+        response = self.database.getCountBookRental()
+        return response
+    @rpc
+    def getCountBookAttraction(self):
+        response = self.database.getCountBookAttraction()
         return response
 
 # Method to add a room
--- dependencies.py
+++ dependencies.py
@@ -39,14 +39,14 @@ class DatabaseWrapper:
         cursor.close()
         return result
 
-    def add_booking_hotel(self,user_id, type, total_price, asuransi_id, hotel_name,
+    def add_booking_hotel(self,user_id, type, total_price, asuransi_id, provider_name,
         room_type, check_in_date, check_out_date,number_of_rooms):
-        result = self.add_booking(user_id=user_id,type=type,total_price=total_price,asuransi_id=asuransi_id)
+        result = self.add_booking(user_id=user_id,type=type,total_price=total_price,asuransi_id=asuransi_id ,provider_name=provider_name)
         if(result['success']):
             try:
                 cursor = self.connection.cursor(dictionary=True)
-                sql = "INSERT INTO `booking_hotels`(`booking_id`, `hotel_name`, `room_type`, `check_in_date`, `check_out_date`, `number_of_rooms`) VALUES (%s, %s, %s, %s, %s, %s)"
-                cursor.execute(sql, (result['new_inserted_id'],hotel_name,room_type,check_in_date,check_out_date,number_of_rooms))
+                sql = "INSERT INTO `booking_hotels`(`booking_id`, `room_type`, `check_in_date`, `check_out_date`, `number_of_rooms`) VALUES (%s, %s, %s, %s, %s)"
+                cursor.execute(sql, (result['new_inserted_id'],room_type,check_in_date,check_out_date,number_of_rooms))
                 self.connection.commit()
                 cursor.close()
                 return {'message': 'Booking created successfully','status': 200}
@@ -56,8 +56,8 @@ class DatabaseWrapper:
         else:
             return {'error': result['error']}
         
-    def add_booking_airline(self,user_id, type, total_price, asuransi_id, flight_id):
-        result = self.add_booking(user_id=user_id,type=type,total_price=total_price,asuransi_id=asuransi_id)
+    def add_booking_airline(self,user_id, type, total_price, asuransi_id, flight_id, provider_name):
+        result = self.add_booking(user_id=user_id,type=type,total_price=total_price,asuransi_id=asuransi_id, provider_name=provider_name)
         if(result['success']):
             try:
                 cursor = self.connection.cursor(dictionary=True)
@@ -72,14 +72,14 @@ class DatabaseWrapper:
         else:
             return {'error': result['error']}
         
-    def add_booking_rental(self,user_id, type, total_price, asuransi_id, rental_provider_name,car_id,pick_up_date,return_date,pick_up_location,
+    def add_booking_rental(self,user_id, type, total_price, asuransi_id, provider_name,car_id,pick_up_date,return_date,pick_up_location,
         return_location,is_with_driver):
-        result = self.add_booking(user_id=user_id,type=type,total_price=total_price,asuransi_id=asuransi_id)
+        result = self.add_booking(user_id=user_id,type=type,total_price=total_price,asuransi_id=asuransi_id, provider_name=provider_name)
         if(result['success']):
             try:
                 cursor = self.connection.cursor(dictionary=True)
-                sql = "INSERT INTO `booking_rentals`(`booking_id`, `rental_provider_name`, `car_id`, `pickup_date`, `return_date`, `pickup_location`, `return_location`, `is_with_driver`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                cursor.execute(sql, (result['new_inserted_id'],rental_provider_name,car_id,pick_up_date,return_date,pick_up_location,return_location,is_with_driver))
+                sql = "INSERT INTO `booking_rentals`(`booking_id`, `car_id`, `pickup_date`, `return_date`, `pickup_location`, `return_location`, `is_with_driver`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (result['new_inserted_id'],car_id,pick_up_date,return_date,pick_up_location,return_location,is_with_driver))
                 self.connection.commit()
                 cursor.close()
                 return {'message': 'Booking created successfully','status': 200}
@@ -89,14 +89,14 @@ class DatabaseWrapper:
         else:
             return {'error': result['error']}
         
-    def add_booking_attraction(self,user_id, type, total_price, asuransi_id, attraction_provider_name,
+    def add_booking_attraction(self,user_id, type, total_price, asuransi_id, provider_name,
         paket_attraction_id, visit_date, number_of_tickets):
-        result = self.add_booking(user_id=user_id,type=type,total_price=total_price,asuransi_id=asuransi_id)
+        result = self.add_booking(user_id=user_id,type=type,total_price=total_price,asuransi_id=asuransi_id, provider_name=provider_name)
         if(result['success']):
             try:
                 cursor = self.connection.cursor(dictionary=True)
-                sql = "INSERT INTO `booking_attractions`(`booking_id`, `attraction_provider_name`, `paket_attraction_id`, `visit_date`, `number_of_tickets`) VALUES (%s, %s, %s, %s, %s)"
-                cursor.execute(sql, (result['new_inserted_id'],attraction_provider_name,paket_attraction_id,visit_date,number_of_tickets))
+                sql = "INSERT INTO `booking_attractions`(`booking_id`, `paket_attraction_id`, `visit_date`, `number_of_tickets`) VALUES (%s, %s, %s, %s)"
+                cursor.execute(sql, (result['new_inserted_id'],paket_attraction_id,visit_date,number_of_tickets))
                 self.connection.commit()
                 cursor.close()
                 return {'message': 'Booking created successfully','status': 200}
@@ -106,7 +106,7 @@ class DatabaseWrapper:
         else:
             return {'error': result['error']}
         
-    def add_booking(self,user_id, type, total_price, asuransi_id):
+    def add_booking(self,user_id, type, total_price, asuransi_id,provider_name):
         try:
             cursor = self.connection.cursor(dictionary=True)
             while(True):
@@ -117,8 +117,8 @@ class DatabaseWrapper:
                 exist_code = cursor.fetchone()
                 if exist_code is None:
                     break
-            sql = "INSERT INTO `bookings`(`user_id`, `booking_type`, `booking_code`,`total_price`, `asuransi_id`) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(sql, (user_id, type, booking_code, total_price, asuransi_id))
+            sql = "INSERT INTO `bookings`(`user_id`, `booking_type`, `booking_code`,`total_price`, `asuransi_id` ,`provider_name`) VALUES (%s,%s, %s, %s, %s, %s)"
+            cursor.execute(sql, (user_id, type, booking_code, total_price, asuransi_id, provider_name))
             self.connection.commit()
             inserted_id = cursor.lastrowid
             cursor.close()
@@ -145,6 +145,73 @@ class DatabaseWrapper:
             error_message = str(e)
             return {'error': error_message}
         
+    def getCountBookHotel(self):
+        try:
+            cursor = self.connection.cursor(dictionary=True)
+            result = []
+            sql = "SELECT provider_name, COUNT(*) AS booking_count FROM bookings WHERE booking_type = 'Hotel' GROUP BY provider_name;"
+            cursor.execute(sql)
+            for row in cursor.fetchall():
+                result.append({
+                    'hotel_name': row['provider_name'],
+                    'booking_count': row['booking_count'],
+                })
+            cursor.close()
+            return result
+        except Exception as e:
+            error_message = str(e)
+            return {'error': error_message, 'status': 500}
+    def getCountBookAirline(self):
+        try:
+            cursor = self.connection.cursor(dictionary=True)
+            result = []
+            sql = "SELECT provider_name, COUNT(*) AS booking_count FROM bookings WHERE booking_type = 'Airline' GROUP BY provider_name;"
+            cursor.execute(sql)
+            for row in cursor.fetchall():
+                result.append({
+                    'airline_name': row['provider_name'],
+                    'booking_count': row['booking_count'],
+                })
+            cursor.close()
+            return result
+        except Exception as e:
+            error_message = str(e)
+            return {'error': error_message, 'status': 500}
+    def getCountBookRental(self):
+        try:
+            cursor = self.connection.cursor(dictionary=True)
+            result = []
+            sql = "SELECT provider_name, COUNT(*) AS booking_count FROM bookings WHERE booking_type = 'Rental' GROUP BY provider_name;"
+            cursor.execute(sql)
+            for row in cursor.fetchall():
+                result.append({
+                    'rental_name': row['provider_name'],
+                    'booking_count': row['booking_count'],
+                })
+            cursor.close()
+            return result
+        except Exception as e:
+            error_message = str(e)
+            return {'error': error_message, 'status': 500}
+        
+    def getCountBookAttraction(self):
+        try:
+            cursor = self.connection.cursor(dictionary=True)
+            result = []
+            sql = "SELECT provider_name, COUNT(*) AS booking_count FROM bookings WHERE booking_type = 'Attraction' GROUP BY provider_name;"
+            cursor.execute(sql)
+            for row in cursor.fetchall():
+                result.append({
+                    'attraction_name': row['provider_name'],
+                    'booking_count': row['booking_count'],
+                })
+            cursor.close()
+            return result
+        except Exception as e:
+            error_message = str(e)
+            return {'error': error_message, 'status': 500}
+        
+
         # Review related methods
     def add_review(self, booking_id, user_id, rating, review_text):
         try:
@@ -351,8 +418,8 @@ class DatabaseWrapper:
 
 
 
-#    def __del__(self):
-#        self.connection.close()
+    def __del__(self):
+       self.connection.close()
 
 
 class Database(DependencyProvider):
