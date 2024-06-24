8fb7b45be545aabeba5ddfd3e012dff6c795959e
Yan Nathanael W <yanhato03@gmail.com>
2024-06-24 17:44:57 +0700

    x

--- testing_api/mysql-init/init-db.sql
+++ testing_api/mysql-init/init-db.sql
@@ -1,59 +1,59 @@
-CREATE TABLE IF NOT EXISTS `bookings` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `user_id` int(10) UNSIGNED NOT NULL,
-  `booking_type` varchar(20) NOT NULL,
-  `booking_date` datetime NOT NULL DEFAULT current_timestamp(),
-  `status` int(11) NOT NULL,
-  `total_price` decimal(10,2) NOT NULL,
-  `asuransi_id` int(10) UNSIGNED DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- CREATE TABLE IF NOT EXISTS `bookings` (
+--   `id` int(10) UNSIGNED NOT NULL,
+--   `user_id` int(10) UNSIGNED NOT NULL,
+--   `booking_type` varchar(20) NOT NULL,
+--   `booking_date` datetime NOT NULL DEFAULT current_timestamp(),
+--   `status` int(11) NOT NULL,
+--   `total_price` decimal(10,2) NOT NULL,
+--   `asuransi_id` int(10) UNSIGNED DEFAULT NULL,
+--   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+--   `updated_at` timestamp NULL DEFAULT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `booking_airlines` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `booking_id` int(10) UNSIGNED NOT NULL,
-  `flight_id` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- CREATE TABLE IF NOT EXISTS `booking_airlines` (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `booking_id` int(10) UNSIGNED NOT NULL,
+--   `flight_id` int(11) NOT NULL,
+--   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+--   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `booking_attractions` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `booking_id` int(10) UNSIGNED NOT NULL,
-  `attraction_provider_name` varchar(255) NOT NULL,
-  `paket_attraction_id` int(10) UNSIGNED NOT NULL,
-  `visit_date` date NOT NULL,
-  `number_of_tickets` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- CREATE TABLE IF NOT EXISTS `booking_attractions` (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `booking_id` int(10) UNSIGNED NOT NULL,
+--   `attraction_provider_name` varchar(255) NOT NULL,
+--   `paket_attraction_id` int(10) UNSIGNED NOT NULL,
+--   `visit_date` date NOT NULL,
+--   `number_of_tickets` int(11) NOT NULL,
+--   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+--   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `booking_hotels` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `booking_id` int(10) UNSIGNED NOT NULL,
-  `hotel_name` varchar(50) NOT NULL,
-  `room_type` int(11) NOT NULL,
-  `check_in_date` date NOT NULL,
-  `check_out_date` date NOT NULL,
-  `number_of_rooms` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- CREATE TABLE IF NOT EXISTS `booking_hotels` (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `booking_id` int(10) UNSIGNED NOT NULL,
+--   `hotel_name` varchar(50) NOT NULL,
+--   `room_type` int(11) NOT NULL,
+--   `check_in_date` date NOT NULL,
+--   `check_out_date` date NOT NULL,
+--   `number_of_rooms` int(11) NOT NULL,
+--   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+--   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `booking_rentals` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `booking_id` int(10) UNSIGNED NOT NULL,
-  `rental_provider_name` varchar(50) NOT NULL,
-  `car_id` int(10) UNSIGNED NOT NULL,
-  `pickup_date` datetime NOT NULL,
-  `return_date` datetime NOT NULL,
-  `pickup_location` varchar(255) NOT NULL,
-  `return_location` varchar(255) NOT NULL,
-  `is_with_driver` tinyint(1) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- CREATE TABLE IF NOT EXISTS `booking_rentals` (
+--   `id` bigint(20) UNSIGNED NOT NULL,
+--   `booking_id` int(10) UNSIGNED NOT NULL,
+--   `rental_provider_name` varchar(50) NOT NULL,
+--   `car_id` int(10) UNSIGNED NOT NULL,
+--   `pickup_date` datetime NOT NULL,
+--   `return_date` datetime NOT NULL,
+--   `pickup_location` varchar(255) NOT NULL,
+--   `return_location` varchar(255) NOT NULL,
+--   `is_with_driver` tinyint(1) NOT NULL,
+--   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+--   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `reviews` (
   `id` int(10) UNSIGNED NOT NULL,
